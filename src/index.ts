726360b5b2ad98d506a84aa290426e0774eb19a6
bergsans <bergclaesmagnus@gmail.com>
2021-09-18 14:23:57 +0200

    Add tokenize and parse to index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+import { tokenize } from './lexer/tokenize';
+import { parse } from './parser/parse';
+// import { evaluate } from './evaluate/evaluate';
 
+export default {
+  tokenize,
+  parse,
+  evaluate: undefined,
+};
