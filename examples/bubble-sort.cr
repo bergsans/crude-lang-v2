47d5e6fa7e17a57b0e021da3ae2fa51fc70cd3c4
bergsans <bergclaesmagnus@gmail.com>
2021-10-13 12:57:10 +0200

    Update code

--- examples/bubble-sort.cr
+++ examples/bubble-sort.cr
@@ -1,24 +1,14 @@
 define bubbleSort(arr) {
-  let len = length(arr);
-  define outerLoop(i, a) {
-    define innerLoop(j, b) {
-      if(j < len) {
-        let prevVal = id(b[j]);
-        let nextVal = id(b[j + 1]);
-        if(prevVal > nextVal) {
-          let tempArr = change(b, j, nextVal);
-          let nextArr = change(tempArr, j + 1, prevVal);
-          return innerLoop(j + 1, nextArr);
-        }
-        return innerLoop(j + 1, b);
+  for(i, 0, length(arr)) {
+    for(j, 0, length(arr)) {
+      let prevVal = arr[j];
+      let nextVal = arr[j + 1];
+      if(prevVal > nextVal) {
+        let arr = change(arr, j, nextVal);
+        return change(arr, j + 1, prevVal);
       }
-      return outerLoop(i + 1, b);
     }
-    if(i < len) {
-      return innerLoop(0, a);
-    }
-    return a;
   }
-  return outerLoop(0, arr);
+  return arr;
 }
 bubbleSort([13, 21, 1, 34, 2, 1, 5, 3, 55, 0, 8]);
