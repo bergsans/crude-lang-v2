a78444185065ecda9f048b48739e88faa4a4118a
bergsans <bergclaesmagnus@gmail.com>
2021-10-13 12:56:51 +0200

    Update fns using for syntax

--- src/std-lib/lib.cr
+++ src/std-lib/lib.cr
@@ -18,40 +18,31 @@ define inc(x) {
   return x + 1;
 }
 
-define filter(predicate, li) {
-  define loop(list, i) {
-    if(i < length(li)) {
-      if(predicate(li[i])) {
-        return loop(concat(list, li[i]), i + 1);
-      }
-      return loop(list, i + 1);
+define filter(predicate, arr) {
+  let filtArr = [];
+  for(i, 0, length(arr)) {
+    if(predicate(arr[i])) {
+      return change(filtArr, length(filtArr), arr[i]);
     }
-    return list;
   }
-  return loop([], 0);
+  return filtArr;
 }
 
-define map(fn, li) {
-  define loop(list, i) {
-    if(i < length(li)) {
-      return loop(concat(list, fn(li[i])), i + 1);
-    }
-    return list;
+define map(fn, arr) {
+  let newArr = [];
+  for(i, 0, length(arr)) {
+    return change(newArr, i, fn(arr[i]));
   }
-  return loop([], 0);
+  return newArr;
 }
 
 define every(predicate, li) {
-  define loop(i) {
-    if(i < length(li)) {
-      if(predicate(li[i]) == false) {
-        return false;
-      }
-      return loop(i + 1);
+  for(i, 0, length(li)) {
+    if(predicate(li[i]) == false) {
+      return false;
     }
-    return true;
   }
-  return loop(0);
+  return true;
 }
 
 define some(predicate, li) {
@@ -86,4 +77,18 @@ define trimStart(s) {
     return count(n + 1);
   }
   return count(0);
+}
+
+define sort(arr) {
+  for(i, 0, length(arr)) {
+    for(j, 0, length(arr)) {
+      let prevVal = arr[j];
+      let nextVal = arr[j + 1];
+      if(prevVal > nextVal) {
+        let arr = change(arr, j, nextVal);
+        return change(arr, j + 1, prevVal);
+      }
+    }
+  }
+  return arr;
 }
