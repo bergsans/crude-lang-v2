0200eb737fe9e3595fedc10764f693d504f607ac
bergsans <bergclaesmagnus@gmail.com>
2021-10-17 15:38:17 +0200

    Update help system

--- src/repl/repl.ts
+++ src/repl/repl.ts
@@ -10,8 +10,7 @@ const repl = createInterface({
   output: process.stdout,
 });
 
-function getHelpTexts() {
-  const msg = `
+const msg = `
  ----------------------------------------------------------------------------------
   _______  ______    __   __  ______   _______  ___      _______  __    _  _______ 
  |       ||    _ |  |  | |  ||      | |       ||   |    |   _   ||  |  | ||       |
@@ -21,29 +20,47 @@ function getHelpTexts() {
  |     |_ |   |  | ||       ||       ||   |___ |       ||   _   || | |   ||   |_| |
  |_______||___|  |_||_______||______| |_______||_______||__| |__||_|  |__||_______|
 
- READ-EVALUATE-PRINT-LOOP
+ READ-EVALUATE-PRINT-LOOP`;
 
- ----------------------------------------------------------------------------------
-`;
-
-  const helpMsg = `
+const helpMsg = `
  ----------------------------------------------------------------------------------
    Exit\t\t :exit
    Help\t\t :help
  ----------------------------------------------------------------------------------
 `;
-  return { msg, helpMsg };
-}
-const { helpMsg, msg } = getHelpTexts();
+
+const examples = `
+> 3 + 3;
+6
+> 2 + 2 == 5;
+false
+> "Hello, " + "World!";
+Hello, World!
+> let name = "Tux";
+undefined
+> name;
+Tux
+> map(inc, [1, 2, 3]);
+[ 2, 3, 4 ]
+> foldl(add, 0, [1, 2, 3, 4, 5]);
+15
+> filter(isOdd, [1, 2, 3, 4, 5]);
+[ 1, 3, 5 ]
+> define appendBang(str) { return str + "!"; }
+undefined
+> appendBang("Hi");
+Hi!
+`;
 
 let replContext = [];
 
 const commands = {
   ':exit': () => process.exit(0),
-  ':help': () => console.log(helpMsg),
+  ':help': () => console.log(examples),
 };
 
 console.log(fmtStr(msg, 'blue', null, 'bold'));
+console.log(fmtStr(helpMsg, 'blue', null, 'bold'));
 
 (async function initWithStdLib() {
   const stdLib = await importStdLib();
